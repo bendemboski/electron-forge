cbd6350d3b6cbd3795ffe723dcb7da0a583899f8
Ben Demboski <ben@turbopatent.com>
2019-09-03 13:21:15 -0700

    Build

---
+++ dist/util/parse-archs.js
@@ -0,0 +1,17 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = parseArchs;
+
+var _targets = require('electron-packager/src/targets');
+
+function parseArchs(platform, declaredArch, electronVersion) {
+  if (declaredArch === 'all') {
+    return (0, _targets.allOfficialArchsForPlatformAndVersion)(platform, electronVersion) || ['x64'];
+  }
+
+  return declaredArch.split(',');
+}
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInV0aWwvcGFyc2UtYXJjaHMuanMiXSwibmFtZXMiOlsicGFyc2VBcmNocyIsInBsYXRmb3JtIiwiZGVjbGFyZWRBcmNoIiwiZWxlY3Ryb25WZXJzaW9uIiwic3BsaXQiXSwibWFwcGluZ3MiOiI7Ozs7O2tCQUV3QkEsVTs7QUFGeEI7O0FBRWUsU0FBU0EsVUFBVCxDQUFvQkMsUUFBcEIsRUFBOEJDLFlBQTlCLEVBQTRDQyxlQUE1QyxFQUE2RDtBQUMxRSxNQUFJRCxpQkFBaUIsS0FBckIsRUFBNEI7QUFDMUIsV0FBTyxvREFBc0NELFFBQXRDLEVBQWdERSxlQUFoRCxLQUFvRSxDQUFDLEtBQUQsQ0FBM0U7QUFDRDs7QUFFRCxTQUFPRCxhQUFhRSxLQUFiLENBQW1CLEdBQW5CLENBQVA7QUFDRCIsImZpbGUiOiJ1dGlsL3BhcnNlLWFyY2hzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgYWxsT2ZmaWNpYWxBcmNoc0ZvclBsYXRmb3JtQW5kVmVyc2lvbiB9IGZyb20gJ2VsZWN0cm9uLXBhY2thZ2VyL3NyYy90YXJnZXRzJztcblxuZXhwb3J0IGRlZmF1bHQgZnVuY3Rpb24gcGFyc2VBcmNocyhwbGF0Zm9ybSwgZGVjbGFyZWRBcmNoLCBlbGVjdHJvblZlcnNpb24pIHtcbiAgaWYgKGRlY2xhcmVkQXJjaCA9PT0gJ2FsbCcpIHtcbiAgICByZXR1cm4gYWxsT2ZmaWNpYWxBcmNoc0ZvclBsYXRmb3JtQW5kVmVyc2lvbihwbGF0Zm9ybSwgZWxlY3Ryb25WZXJzaW9uKSB8fCBbJ3g2NCddO1xuICB9XG5cbiAgcmV0dXJuIGRlY2xhcmVkQXJjaC5zcGxpdCgnLCcpO1xufVxuIl19
